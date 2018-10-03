631c48f0f2a1a9fc2c62b7b3c5e6eb273c80416b
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2018-10-03 16:02:30 +0700

    Refactor Code

--- src/TweetyCore/Controllers/HomeController.cs
+++ src/TweetyCore/Controllers/HomeController.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Tweetinvi;
-using Tweetinvi.Core.Extensions;
-using Tweetinvi.Models;
 using Tweety.Models;
 using TweetyCore.Models;
-using Utils.StringMatching;
+using TweetyCore.Utils;
 
 namespace TweetyCore.Controllers
 {
     public class HomeController : Controller
-    {
-        private int sumOfTweet;
+    { 
 
         public IActionResult Index()
         {
@@ -27,217 +20,24 @@ namespace TweetyCore.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(Tags tag)
+        public IActionResult Index(Tags tags)
         {
             if (ModelState.IsValid)
             {
-                TweetResult tweetResults = new TweetResult
-                {
-                    Query = new List<QueryCategory>()
-                };
-                QueryCategory dinasKesehatan = new QueryCategory
-                {
-                    Id = tag.DinasKesehatan,
-                    Name = "Dinas Kesehatan",
-                    Num = 0,
-                    Tweet = new List<HasilTweet>()
-                };
-                QueryCategory dinasBinamarga = new QueryCategory
-                {
-                    Id = tag.DinasBinamarga,
-                    Name = "Dinas Binamarga",
-                    Num = 0,
-                    Tweet = new List<HasilTweet>()
-                };
-                QueryCategory dinasPemuda = new QueryCategory
-                {
-                    Id = tag.DinasPemuda,
-                    Name = "Dinas Pemuda",
-                    Num = 0,
-                    Tweet = new List<HasilTweet>()
-                };
-                QueryCategory dinasPendidikan = new QueryCategory
-                {
-                    Id = tag.DinasPendidikan,
-                    Name = "Dinas Pendidikan",
-                    Num = 0,
-                    Tweet = new List<HasilTweet>()
-                };
-                QueryCategory dinasSosial = new QueryCategory
-                {
-                    Id = tag.DinasSosial,
-                    Name = "Dinas Sosial",
-                    Num = 0,
-                    Tweet = new List<HasilTweet>()
-                };
+                TwitterConnect twitterConnect = new TwitterConnect();
+                var result = twitterConnect.ProcessTag(tags);
 
-                tweetResults.Query.Add(dinasKesehatan);
-                tweetResults.Query.Add(dinasBinamarga);
-                tweetResults.Query.Add(dinasPemuda);
-                tweetResults.Query.Add(dinasPendidikan);
-                tweetResults.Query.Add(dinasSosial);
-
-                ParseTag(tag, tweetResults);
-
-                if (sumOfTweet > 0)
+                if (result.Item1 > 0)
                 {
-                    return View("ShowResult", tweetResults);
+                    return View("ShowResult", result.Item2);
                 }
                 else
                 {
                     return View("NoResult");
-                }
+                };
+
             }
             return View();
-        }
-
-        private void ParseTag(Tags tag, TweetResult Tweetx)
-        {
-            ITweet[] Tweet = new ITweet[100];
-
-            int[] DKes = new int[100];
-            int idx = 0;
-            string customer_key = Environment.GetEnvironmentVariable("CUSTOMER_KEY");
-            string customer_secret = Environment.GetEnvironmentVariable("CUSTOMER_SECRET");
-            string token = Environment.GetEnvironmentVariable("TOKEN");
-            string token_secret = Environment.GetEnvironmentVariable("TOKEN_SECRET");
-            // When a new thread is created, the default credentials will be the Application Credentials
-            Auth.ApplicationCredentials = new TwitterCredentials(customer_key, customer_secret, token, token_secret);
-            var searchParameter = Search.CreateTweetSearchParameter(tag.Name);
-            searchParameter.MaximumNumberOfResults = 100;
-            sumOfTweet = 0;
-
-            QueryCategory NoCategory = new QueryCategory
-            {
-                Id="no_category",
-                Name = "No Category"
-            };
-            var tweets = Search.SearchTweets(searchParameter);
-            if (tweets != null)
-            {
-                sumOfTweet = tweets.Count();
-                tweets.ForEach(t => InsertT(t, Tweet, ref idx));
-                bool[] NoCate = new bool[sumOfTweet];
-                for (int g = 0; g < sumOfTweet; g++)
-                {
-                    NoCate[g] = false;
-                }
-                foreach (QueryCategory value in Tweetx.Query)
-                {
-                    GetQuery(value.Id, Tweet, value, ref NoCate, tag);
-                }
-                for (int g = 0; g < sumOfTweet; g++)
-                {
-                    if (!NoCate[g])
-                    {
-                        HasilTweet HasilTemp = new HasilTweet
-                        {
-                            TweetContent = Tweet[g],
-                            Result = Tweet[g].Text
-                        };
-                        NoCategory.Tweet.Add(HasilTemp);
-                    }
-                }
-            }
-            Tweetx.Query.Add(NoCategory);
-        }
-
-
-        private void InsertT(ITweet T, ITweet[] Tx, ref int i)
-        {
-            if (i < sumOfTweet)
-            {
-                Tx[i] = T;
-                i++;
-            }
-        }
-
-        private void GetQuery(string Query, ITweet[] Tweet, QueryCategory A, ref bool[] N, Tags tag)
-        {
-            int k;
-            int l;
-            for (int j = 0; j < sumOfTweet; j++)
-            {
-                string queryX = Query;
-                string querySearch;
-                bool queryBool = false;
-                k = 0;
-                l = 0;
-                int X = -1;
-                List<int> q = new List<int>();
-                List<int> ql = new List<int>();
-                if (queryX != null)
-                {
-                    string theQuery = "";
-                    while (k < queryX.Length)
-                    {
-                        bool qfound = false;
-                        while (queryX[k] == ' ' && k < queryX.Length)
-                        {
-                            k++;
-                        }
-                        int x = k;
-                        while (!qfound && k < queryX.Length)
-                        {
-                            if (queryX[k] == ',')
-                            {
-                                qfound = true;
-                            }
-                            k++;
-                        }
-                        if (qfound)
-                        {
-                            querySearch = queryX.Substring(x, k - x - 1);
-                        }
-                        else
-                        {
-                            querySearch = queryX.Substring(x, k - x);
-                        }
-                        if (tag.isKMP)
-                        {
-                            X = KMP.Solve(Tweet[j].Text, querySearch);
-                        }
-                        else
-                        {
-                            X = Booyer.Solve(Tweet[j].Text, querySearch);
-                        }
-                        theQuery = querySearch;
-                        q.Add(X);
-                        ql.Add(querySearch.Length);
-                        if (X != -1)
-                        {
-                            queryBool = true;
-                        }
-                        l++;
-                    }
-                    if (queryBool)
-                    {
-                        N[j] = true;
-                        HasilTweet Ax = new HasilTweet();
-                        foreach (int value in q)
-                        {
-                            Ax.StartMark.Add(value);
-                        }
-                        foreach (int value in ql)
-                        {
-                            Ax.QueryLength.Add(value);
-                        }
-                        Ax.TweetContent = Tweet[j];
-                        A.Tweet.Add(Ax);
-                        A.Num++;
-                        Ax.Result = Ax.TweetContent.Text;
-                        foreach (int pos in Ax.StartMark)
-                        {
-                            if (pos > 0)
-                            {
-                                int i = Ax.StartMark.IndexOf(pos);
-                                string n = Ax.Result.Substring(pos, Ax.QueryLength.ElementAt(i));
-                                Ax.Result = Ax.Result.Replace(n, "<strong>" + n + "</strong>");
-                            }
-                        }
-                    }
-                }
-            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ src/TweetyCore/Utils/TwitterConnect.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Tweetinvi;
+using Tweetinvi.Core.Extensions;
+using Tweetinvi.Models;
+using Tweety.Models;
+using Utils.StringMatching;
+
+namespace TweetyCore.Utils
+{
+    public class TwitterConnect
+    {
+        private TweetResult tweetResults;
+        private QueryCategory dinasKesehatan;
+        private QueryCategory dinasBinamarga;
+        private QueryCategory dinasPemuda;
+        private QueryCategory dinasPendidikan;
+        private QueryCategory dinasSosial;
+        private QueryCategory other;
+        private bool[] categorized;
+
+        public TwitterConnect()
+        {
+            tweetResults = new TweetResult
+            {
+                Query = new List<QueryCategory>()
+            };
+            dinasKesehatan = new QueryCategory
+            {
+                Id = "dinas_kesehatan",
+                Name = "Dinas Kesehatan",
+                Num = 0,
+                Tweet = new List<HasilTweet>()
+            };
+            dinasBinamarga = new QueryCategory
+            {
+                Id = "dinas_binamarga",
+                Name = "Dinas Binamarga",
+                Num = 0,
+                Tweet = new List<HasilTweet>()
+            };
+            dinasPemuda = new QueryCategory
+            {
+                Id = "dinas_pemuda",
+                Name = "Dinas Pemuda",
+                Num = 0,
+                Tweet = new List<HasilTweet>()
+            };
+            dinasPendidikan = new QueryCategory
+            {
+                Id = "dinas_pendidikan",
+                Name = "Dinas Pendidikan",
+                Num = 0,
+                Tweet = new List<HasilTweet>()
+            };
+            dinasSosial = new QueryCategory
+            {
+                Id = "dinas_sosial",
+                Name = "Dinas Sosial",
+                Num = 0,
+                Tweet = new List<HasilTweet>()
+            };
+            other = new QueryCategory
+            {
+                Id = "no_category",
+                Name = "No Category",
+                Num = 0,
+                Tweet = new List<HasilTweet>()
+            };
+            tweetResults.Query.Add(dinasKesehatan);
+            tweetResults.Query.Add(dinasBinamarga);
+            tweetResults.Query.Add(dinasPemuda);
+            tweetResults.Query.Add(dinasPendidikan);
+            tweetResults.Query.Add(dinasSosial);
+            tweetResults.Query.Add(other);
+        }
+
+        private void Connect()
+        {
+            string customer_key = Environment.GetEnvironmentVariable("CUSTOMER_KEY");
+            string customer_secret = Environment.GetEnvironmentVariable("CUSTOMER_SECRET");
+            string token = Environment.GetEnvironmentVariable("TOKEN");
+            string token_secret = Environment.GetEnvironmentVariable("TOKEN_SECRET");
+            // When a new thread is created, the default credentials will be the Application Credentials
+            Auth.ApplicationCredentials = new TwitterCredentials(customer_key, customer_secret, token, token_secret);
+        }
+
+        public Tuple<int, TweetResult> ProcessTag(Tags tags)
+        {
+            int sumOfTweets = ParseTag(tags);
+            return Tuple.Create(sumOfTweets, tweetResults);
+        }
+
+        private int ParseTag(Tags tag)
+        {
+            int sumOfTweet = 0;
+            Connect();
+            var searchParameter = Search.CreateTweetSearchParameter(tag.Name);
+            searchParameter.MaximumNumberOfResults = 100;
+            var tweets = Search.SearchTweets(searchParameter);
+            if (tweets != null)
+            {
+                sumOfTweet = tweets.Count();
+                categorized = new bool[sumOfTweet];
+                for (int i = 0; i < sumOfTweet; i++)
+                {
+                    categorized[i] = false;
+                }
+
+                foreach (QueryCategory category in tweetResults.Query)
+                {
+                    string keywords = "";
+                    if (category.Id == "dinas_kesehatan")
+                    {
+                        keywords = tag.DinasKesehatan;
+                    }
+                    else if (category.Id == "dinas_binamarga")
+                    {
+                        keywords = tag.DinasBinamarga;
+                    }
+                    else if (category.Id == "dinas_pendidikan")
+                    {
+                        keywords = tag.DinasPendidikan;
+                    }
+                    else if (category.Id == "dinas_pemuda")
+                    {
+                        keywords = tag.DinasPemuda;
+                    }
+                    else if (category.Id == "dinas_sosial")
+                    {
+                        keywords = tag.DinasSosial;
+                    }
+                    if (keywords != "")
+                    {
+                        GetQuery(category, tweets, keywords, tag.isKMP);
+                    }
+                }
+                for (int j = 0; j < sumOfTweet; j++)
+                {
+                    if (!categorized[j])
+                    {
+                        HasilTweet hasilTemp = new HasilTweet
+                        {
+                            TweetContent = tweets.ElementAt(j),
+                            Result = tweets.ElementAt(j).Text
+                        };
+                        tweetResults.Query.Find(query => query.Id == "no_category").Tweet.Add(hasilTemp);
+                    }
+                }
+            }
+            return sumOfTweet;
+        }
+
+        private void GetQuery(QueryCategory category, IEnumerable<ITweet> tweets, string keywords, bool isKMP)
+        {
+            string[] keywordsArray = keywords.Split(",");
+            int i = 0;
+            foreach (ITweet tweet in tweets)
+            {
+                int indexFound;
+                int cats = 0;
+                string newText = tweet.Text;
+                foreach (string keyWord in keywordsArray)
+                {
+                    indexFound = -1;
+                    if (isKMP)
+                    {
+                        indexFound = KMP.Solve(tweet.Text, keyWord);
+                    }
+                    else
+                    {
+                        indexFound = Booyer.Solve(tweet.Text, keyWord);
+                    }
+                    if (indexFound != -1)
+                    {
+                        cats++;
+                        categorized[i] = true;
+                        newText = Regex.Replace(newText, keyWord, @"<b>$&</b>", RegexOptions.IgnoreCase);
+                    }
+                }
+                if (cats > 0)
+                {
+                    HasilTweet hasil = new HasilTweet
+                    {
+                        TweetContent = tweet,
+                        Result = newText
+                    };
+                    tweetResults.Query.Find(q => q.Id == category.Id).Tweet.Add(hasil);
+                }
+                i++;
+            }
+        }
+    }
+}
+
